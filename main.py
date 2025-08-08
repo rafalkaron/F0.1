08bc63e1bef5286d391c0906eb43a535ed463309
rafalkaron <rafalkaron@gmail.com>
2025-08-08 10:14:33 +0200

    Hotfix

--- main.py
+++ main.py
@@ -78,13 +78,6 @@ class F01:
             pass
         await self.led_internal.on()
 
-    async def monitor_ap(self) -> None:
-        while True:
-            if not self.ap.ap.active():
-                print("[AP] Lost connection, re-enabling...")
-                self.ap.ap.active(True)
-            await asyncio.sleep(2)
-
     async def run(self) -> None:
         print("Running F0.1...")
         await asyncio.gather(
